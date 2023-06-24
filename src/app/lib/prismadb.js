467beda5300dc7882062b0cbb807c46b237c1730
mdtanvirgit <freelencertanvirhossain@gmail.com>
2023-06-24 22:01:44 +0600

    I am fix my bug

--- src/app/lib/prismadb.js
+++ src/app/lib/prismadb.js
@@ -1,9 +1,9 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
+const globalForPrisma = globalThis;
+globalForPrisma.prisma = globalForPrisma.prisma ?? new PrismaClient();
 
+if (process.env.NODE_ENV !== 'production') {
+    globalForPrisma.prisma = globalForPrisma.prisma;
+}
 
-const client = globalThis.prisma || new PrismaClient()
-
-
-if (process.env.NODE_ENV === "production") globalThis.prisma = client;
-
-export default client;
+export const prisma = globalForPrisma.prisma;
